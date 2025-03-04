78f134c7cca8fe95edb7e36da77d36ab9a0ff2be
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-04 14:33:12 +0300

    Son düzenlemeler yapıldı

--- src/main/java/bodyindex/bodyindexcalculater.java
+++ src/main/java/bodyindex/bodyindexcalculater.java
@@ -6,19 +6,19 @@ public class bodyindexcalculater {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Kullancının veri girmesi için gerekli.
 
         System.out.println("Lütfen Boyunuzu giriniz(metre cinsinden) : ");
 
-        double boy = scanner.nextDouble();
+        double boy = scanner.nextDouble();  // Kullanıcıdan boy girdisi isteniyor
 
         System.out.println("Lütfen Kilonuzu giriniz : ");
 
-        double kg = scanner.nextDouble();
+        double kg = scanner.nextDouble(); //Kullanıcıdan kilogram girdisi isteniyor
 
-        double v = kg/(boy * boy) ;
+        double index = kg/(boy * boy) ; // İndex Alma işlemi = Kilogram / Boyunuzun karesi
 
-        System.out.println("Vücut Kitle İndeksiniz = " + v);
+        System.out.println("Vücut Kitle İndeksiniz = " + index); // işlem sonucu yazdırma
 
 
 
