dd3f5a83a15808321f8cd6ed1301629b7b52ff3f
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-04 17:19:01 +0300

    Manav kasa programı v1.0

--- src/main/java/cratecalculator.java
+++ src/main/java/cratecalculator.java
@@ -12,30 +12,28 @@ public class cratecalculator {
         // muzkg = 0,95 TL
         // patlıcankg = 5,00 TL
 
-        System.out.println("Armut kaç Kilo ? ");
+        System.out.print("Armut kaç Kilo ? ");
             double armutkg = scanner.nextDouble();
             double atl =  (armutkg * 2.14);                    //atl = Armut TL (sonuç)
 
 
-        System.out.println("Elma kaç Kilo ? ");
+        System.out.print("Elma kaç Kilo ? ");
         double elmakg = scanner.nextDouble();                  //etl = Elma TL (sonuç)
         double etl =  (elmakg * 3.67);
 
 
-        System.out.println("Domates kaç Kilo ? ");
+        System.out.print("Domates kaç Kilo ? ");
         double Domateskg = scanner.nextDouble();               //dtl = Domates TL (sonuç)
         double dtl =  (Domateskg * 1.11);
 
 
-        System.out.println("Muz kaç Kilo ? ");
+        System.out.print("Muz kaç Kilo ? ");
         double Muzkg = scanner.nextDouble();                   //mtl = Muz TL (sonuç)
         double mtl =  (Muzkg * 0.95);
 
 
-        System.out.println("Patlıcan kaç Kilo ? ");
-            double patlicantkg = scanner.nextDouble();         //ptl = Patlican TL (sonuç)
-
-
+        System.out.print("Patlıcan kaç Kilo ? ");
+        double patlicantkg = scanner.nextDouble();         //ptl = Patlican TL (sonuç)
         double ptl =  (patlicantkg * 5.00);
 
         double result = atl + etl + dtl + mtl + ptl ;         //result = Toplam Tutar
