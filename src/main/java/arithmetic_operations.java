52eece7821c1ff29c58a2716e525d4dc12badd0d
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-05 18:59:48 +0300

    Last Update.

--- src/main/java/arithmetic_operations.java
+++ src/main/java/arithmetic_operations.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class arithmetic_operations {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //Kullanıcıdan girdi için gerekli method
+
+        System.out.print("1.Sayıyı giriniz : ");
+        double num1 = scanner.nextDouble();      //Kullanıcıdan sayı girmesi için gerekli.
+
+        System.out.print("2.Sayıyı giriniz : ");
+        double num2 = scanner.nextDouble();      //Kullanıcıdan sayı girmesi için gerekli.
+
+        System.out.print("3.Sayıyı giriniz : ");
+        double num3 = scanner.nextDouble();      //Kullanıcıdan sayı girmesi için gerekli.
+
+        System.out.println( num1 + (num2*num3) - num2); // Aritmetik işlemi yapan kod satırı.
+
+
+    }
 }
