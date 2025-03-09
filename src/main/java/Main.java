35408f72dc78afb8d2fae626144aa3d266d9a081
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-09 18:04:18 +0300

    İnverted_Triangle v1.0

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ters üçgen Kaç basamaklı olsun ? ");
+        int number_of_digits = scanner.nextInt();
+
+        for (int i = number_of_digits; i >= 1 ; i--) { // ter üçgen için kullanılan döngü
+            //Her satırda i kadar yıldız yazılıyor
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); // Her satırdan sonra yeni satıra geçmek için.
+
+        }
+
+
+    }
+
+}
