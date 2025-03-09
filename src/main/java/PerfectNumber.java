037ee0e1154c2c729d6dd721d3e9ad705ccd8437
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-09 17:41:33 +0300

    Perfect Number v1.00

--- src/main/java/PerfectNumber.java
+++ src/main/java/PerfectNumber.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PerfectNumber {
+
+    public static void main(String[] args) {
+
+        // // Kullanıcıdan sayıyı almak için Scanner ekliyoruz.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        int number = scanner.nextInt();
+
+        //Sayının mükemmel olup olmadığını kontrol ediyoruz
+        int toplam = 0;
+
+        // Sayının pozitif tam sayı çarpanlarını buluyoruz
+        for (int i = 1; i < number; i++) { //Eğer i , sayının böleniyse i'yi toplamı ekliyoruz
+            if (number % i == 0  ){
+                toplam += i;
+
+            }
+
+        }
+        // Mükemmel sayı kontrolü
+        if(toplam == number){
+            System.out.println("Mükkemel sayıdır.");
+        }else{
+            System.out.println("Mükemmel sayı değildir.");
+        }
+    }
 }
