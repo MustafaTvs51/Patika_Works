72caa8a1328999c7db55bec3b2db7d0ada66f740
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-12 16:28:05 +0300

    Initial commit

---
+++ src/main/java/casting_practice.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class casting_practice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Bir tam sayı giriniz  : ");//Kullanıcıdan girdi alıyoruz.
+        int tamsayi = scanner.nextInt();
+
+        System.out.print("Bir ondalıklı sayı giriniz : ");//Kullanıcıdan girdi alıyoruz.
+        double ondalıklısayi = scanner.nextDouble();
+
+        double castondalıklısayi = tamsayi; //Tam sayıyı ondalıklı hale getiriyor
+        int casttamsayi = (int)ondalıklısayi; //ondalıklı sayıyı tam sayı haline getiriyor
+
+        System.out.println("Tam sayının ondalık hali : " + castondalıklısayi);
+        System.out.println("Ondalık sayının tam sayı hali : " + casttamsayi);
+
+    }
+
+}
