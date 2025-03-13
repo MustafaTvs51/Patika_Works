364fb681c28617182eba35687ff76c9f9732d70a
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-13 05:12:29 +0300

    Final v1.0

--- src/main/java/RoundedNumMain.java
+++ src/main/java/RoundedNumMain.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RoundedNumMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen Ondalıklı bir sayı giriniz : ");
+        double number = scanner.nextDouble(); //Kullanıcıdan ondalıklı sayı alma.
+
+        System.out.println("Aşağıı yuvarlama :" + Math.floor(number)); //Aşağı Yuvarlama metodu
+        System.out.println("Yukarı yuvarlama :" + Math.ceil(number));  // Yukarı Yuvarlama metodu
+        System.out.println("En yakın tam sayı :" + Math.round(number)); // Yuvarlama metodu.
+
+        scanner.close();
+    }
 }
