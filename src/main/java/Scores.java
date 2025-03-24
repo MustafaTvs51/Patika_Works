b8fb706fffebf52d4523a4bc0aa58f26948ec0e8
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-24 15:35:06 +0300

    Initial commit

---
+++ src/main/java/Scores.java
@@ -0,0 +1,15 @@
+public class Scores {
+
+
+    Integer scores;
+
+    public Scores( Integer scores) {
+
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return " " + "=" + " " + scores;
+    }
+}
