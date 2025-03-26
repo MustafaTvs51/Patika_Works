c93687643064566614a4d58150e20ab39ffcbb0b
tvs51 <mustafa_Tavusbay@outlook.com>
2025-03-26 18:55:40 +0300

    Initial commit

---
+++ src/main/java/GenericMain.java
@@ -0,0 +1,26 @@
+public class GenericMethod {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+    public void printArray(){
+
+        Generic<String> stringArray = new Generic<>();
+        System.out.println(stringArray);
+
+        Generic<Integer> integerArray = new Generic<>();
+        System.out.println(integerArray);
+
+        Generic<Double> doubleArray = new Generic<>();
+        System.out.println(doubleArray);
+
+
+
+    }
+
+
+}
