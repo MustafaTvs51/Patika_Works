145e3d73ea69c7b41842826fa4a01a3aa1b38aff
tvs51 <mustafa_tavusbay@outlook.com>
2025-04-24 16:59:59 +0300

    PrintingNames v1.0

--- src/main/java/MethodReference/PrintingNames.java
+++ src/main/java/MethodReference/PrintingNames.java
@@ -1,4 +1,18 @@
 package MethodReference;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PrintingNames {
+
+    public static void main(String[] args) {
+
+        // Bir String listesi oluşturuluyor ve isimleri ekliyoruz.
+        List<String> names = Arrays.asList("Mehmet ", "Zeynep", "Ayşe ", "Ahmet");
+
+        // Her bir ismi konsola yazdırmak için method referansı kullanılıyoruz.
+        // System.out::println, her elemanı println metoduna geçirerek yazdıyoruz.
+        names.forEach(System.out::println);
+    }
 }
+
